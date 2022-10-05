3054bd63e278e3f3e4a3e818b456f2699031183f
TelRich <okorogoodrich@gmail.com>
2022-10-05 23:18:15 +0100

    removed unwanted script

--- divine.py
+++
@@ -1,7 +0,0 @@
-# HAPPY BIRTHDAY DIVINEDONTUS
-
-good_things = ['Happiness', 'Peace', 'Everything Good']
-for all in good_things:
-    print(f"{all} is yours, D'Dontus")
-print('='*30)
-print("Enjoy your moment")

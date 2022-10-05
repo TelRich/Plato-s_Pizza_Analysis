ba98f49b9392a66ab6f6aefdd328c95b4cb88433
TelRich <okorogoodrich@gmail.com>
2022-10-05 06:33:50 +0100

    remove divine.py

---
+++ divine.py
@@ -0,0 +1,7 @@
+# HAPPY BIRTHDAY DIVINEDONTUS
+
+good_things = ['Happiness', 'Peace', 'Everything Good']
+for all in good_things:
+    print(f"{all} is yours, D'Dontus")
+print('='*30)
+print("Enjoy your moment")
